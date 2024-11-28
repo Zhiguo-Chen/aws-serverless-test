43b2db18b49b0f263fa7b6e009ca976f106ef212
chenzhiguo <chenzhiguo91@gmail.com>
2024-11-28 12:17:01 +0800

    Clear

--- backend/src/utils/loadData.js
+++ backend/src/utils/loadData.js
@@ -2,8 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
-const Category = require('../models/Category');
-// const Product = require('../models/Product');
 const sequelize = require('../config/database');
 
 const loadData = async (Model, fileName) => {
@@ -38,33 +36,6 @@ const loadData = async (Model, fileName) => {
       await sequelize.close();
       console.log('Database connection closed.');
     }
-    // fs.createReadStream(path.resolve(__dirname + '/../db/data/', fileName))
-    //   .pipe(csv())
-    //   .on('data', (data) => results.push(data))
-    //   .on('end', async () => {
-    //     console.log('===============++============');
-    //     console.log(results);
-    //     console.log('===============++============');
-
-    //     try {
-    //       // Insert data into the database
-    //       for (const row of results) {
-    //         await Model.create({
-    //           name: row.categories,
-    //         });
-    //       }
-    //       console.log('Data seeded successfully.');
-    //     } catch (error) {
-    //       console.error('Error inserting data:', error);
-    //     } finally {
-    //       await sequelize.close();
-    //     }
-    //   }
-    // )
-    // .on('error', (error) => {
-    //   console.error('Error reading CSV file:', error);
-    //   sequelize.close();
-    // });
   } catch (error) {
     console.error('Error seeding data:', error);
     await sequelize.close();
