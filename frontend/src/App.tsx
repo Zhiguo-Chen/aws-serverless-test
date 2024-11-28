bed2f861248b5f2025c7ef2860cd7995706cfdf7
chenzhiguo <chenzhiguo91@gmail.com>
2024-11-28 11:57:29 +0800

    Add router guard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./pages/register/register";
-import Login from "./pages/login/Login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './pages/register/register';
+import Login from './pages/login/Login';
+import MainLayout from './components/main-layout/main-layout';
+import AddProducts from './pages/add-products/Add-Products';
+import ProtectedRoute from './components/protected-route/protected-route';
 
 const App = () => (
   <div data-mode="light">
@@ -8,7 +11,11 @@ const App = () => (
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/main" />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/main" element={<MainLayout />}>
+            <Route path="add-product" element={<AddProducts />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </div>
