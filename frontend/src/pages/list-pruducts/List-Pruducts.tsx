b10fba5c15405791698d7f79ea0dc27ee5b5e324
chenzhiguo <chenzhiguo91@gmail.com>
2024-12-20 18:00:14 +0800

    Add product list

--- frontend/src/pages/list-pruducts/List-Pruducts.tsx
+++ frontend/src/pages/list-pruducts/List-Pruducts.tsx
@@ -1,43 +1,97 @@
-import { useEffect, useState } from 'react';
-import { axiosInstance } from '../../auth/axiosInstance';
 import { Card } from 'antd';
+import { AppstoreOutlined, BarsOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import ProductBrick from '../../components/ProductBrick/ProductBrick';
+import ProductItem from '../../components/ProductItem/ProductItem';
+
+export interface Product {
+  category_id: number;
+  createdAt: Date;
+  description: string;
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  stock: number;
+  updatedAt: Date;
+  user_id: string;
+  Category: {
+    id: number;
+    name: string;
+  };
+}
 
 const ListProducts = ({
   products,
   onProductClick,
 }: {
-  products: any;
+  products: Array<Product>;
   onProductClick: (product: any) => void;
 }) => {
+  const [isGrid, setIsGrid] = useState(true);
+  const gridStyle = {
+    maxHeight: '270px',
+    width: 'auto',
+    maxWidth: '240px',
+  };
+  const listStyle = {
+    maxHeight: '110px',
+    width: 'auto',
+  };
   return (
-    <ul className="grid auto-fit">
-      {products.map((product: any) => {
-        return (
-          <li
-            key={product.id}
-            style={{ height: '20rem' }}
-            onClick={onProductClick}
-          >
-            {/* <img
-              src={product.image}
-              alt={product.name}
-              width={'100%'}
-              height={'240px'}
-            />
-            <div>{product.name}</div> */}
-            <Card style={{ width: '100%' }}>
-              <img
-                src={product.image}
-                alt={product.name}
-                width={'100%'}
-                height={'240px'}
-              />
-              <p>{product.name}</p>
-            </Card>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <div className="flex justify-end items-center padding-1 flex-gap">
+        <BarsOutlined
+          onClick={() => setIsGrid(false)}
+          style={{
+            fontSize: '24px',
+            cursor: 'pointer',
+            ...(!isGrid && { color: '#ff5000' }),
+          }}
+        />
+        <AppstoreOutlined
+          onClick={() => setIsGrid(true)}
+          style={{
+            fontSize: '24px',
+            cursor: 'pointer',
+            ...(isGrid && { color: '#ff5000' }),
+          }}
+        />
+      </div>
+      <ul
+        className={
+          isGrid
+            ? 'grid auto-fit grid-gap padding-1'
+            : 'flex flex-column flex-gap padding-1'
+        }
+      >
+        {products.map((product: Product) => {
+          return (
+            <li
+              key={product.id}
+              style={{ height: isGrid ? '30rem' : '10rem' }}
+              onClick={onProductClick}
+            >
+              {/* <Card style={{ width: '100%' }}>
+                <div>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    style={isGrid ? gridStyle : listStyle}
+                  />
+                  {isGrid ?? <div>{product.name}</div>}
+                </div>
+              </Card> */}
+              {isGrid ? (
+                <ProductBrick product={product} />
+              ) : (
+                <ProductItem product={product} />
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
