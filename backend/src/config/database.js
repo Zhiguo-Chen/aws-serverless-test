20c57cfc9b03c31f1cfc536b052fd9f5b23171e6
chenzhiguo <chenzhiguo91@gmail.com>
2025-05-28 13:07:00 +0800

    Update db connections

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,16 +2,10 @@ import 'dotenv/config';
 import pg from 'pg';
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    dialectModule: pg,
-  },
-);
+const sequelize = new Sequelize(process.env.DB_URL, {
+  logging: false,
+  dialect: 'postgres',
+  dialectModule: pg,
+});
 
 export default sequelize;
