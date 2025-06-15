b909cdb948a6ce0653c5c1c4eea943790b567a66
chenzhiguo <chenzhiguo91@gmail.com>
2025-06-15 21:22:34 +0800

    Transfer js to ts

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -1,34 +1,32 @@
-import db from '../models/index.js';
+import db, { Category } from '../models';
+import { Request, Response } from 'express';
 
-export const listAllCategory = async (req, res) => {
-  const { Category } = db;
+export const listAllCategory = async (req: Request, res: Response) => {
   try {
     const category = await Category.findAll();
     return res.status(200).json(category);
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-export const getAllCategories = async (req, res) => {
-  const { Category } = db;
+export const getAllCategories = async (req: Request, res: Response) => {
   try {
     const categories = await Category.findAll({
       where: { isActive: true },
       attributes: ['id', 'name', 'slug', 'imageUrl'],
     });
     res.status(200).json(categories);
-  } catch (error) {
+  } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
 };
 
-export const createCategory = async (req, res) => {
-  const { Category } = db;
+export const createCategory = async (req: Request, res: Response) => {
   try {
     const category = await Category.create(req.body);
     res.status(201).json(category);
-  } catch (error) {
+  } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
 };
